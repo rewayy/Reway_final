2a9b23f9317ada105d0802479e261561ef4c9abd
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-19 15:25:06 +0530

    add documentation

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,9 +1,7 @@
-// import logo from './svgs/logo.svg';
-// / Import your PNG files
-// import hero from './images/Hero-img/hero.png';
-// import slide_image_1 from './images/members/img-1.jpeg';
 
 
+//importing images
 import logo from '../assets/logo.jpeg';
 
+//exporting images
 export {logo,};
