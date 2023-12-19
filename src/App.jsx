dac0f8429b57902b277c65177e21c71d7ab908ee
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-19 17:21:16 +0530

    add OurJourney component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { HeroPage, } from './components'
+import { HeroPage, OurJourney, SustainableGoals} from './components/index'
 
 export default function App() {
   return (
     <>
       <HeroPage/>
+      <OurJourney/>
+      <SustainableGoals/>
     </>
   )
 }
