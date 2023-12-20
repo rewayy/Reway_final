5e682a265cb60496319c6168ecd79970becd44a1
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-20 13:39:45 +0530

    add card-imgs

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,11 @@
 import logo from '../assets/logo.jpeg';
 import wasteImage from '../assets/wasteImage.png';
 import youtube from '../assets/youtube.png';
+import E_PRINT_12 from '../assets/E_PRINT_12.jpg';
+import E_PRINT_17 from '../assets/E_PRINT_17.jpg';
+import E_PRINT_15 from '../assets/E_PRINT_15.jpg';
+import E_WEB_13 from '../assets/E_WEB_13.png';
+import sync from '../assets/sync.png';
 
 //exporting images
-export {logo,wasteImage, youtube};
+export {logo,wasteImage, youtube, E_PRINT_12, E_WEB_13, E_PRINT_15, E_PRINT_17, sync};
