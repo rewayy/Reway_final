0926ada253eaccfb2479c82db30ccc5951e07c15
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-20 13:43:47 +0530

    included FCards.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import { HeroPage, OurJourney, SustainableGoals, Thumbnail} from './components/index'
+import { FCards, HeroPage, OurJourney, Thumbnail} from './components/index'
 
 export default function App() {
   return (
     <>
       <HeroPage/>
       <OurJourney/>
-      {/* <SustainableGoals/> */}
+      <FCards/>
       <Thumbnail/>
     </>
   )
