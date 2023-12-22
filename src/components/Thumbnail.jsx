7355003fd0d744dd953142f40bae4f27728267cf
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-22 13:21:15 +0530

    modified thumbnail.jsx

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -5,22 +5,23 @@ import '../components/Card/FCards.css';
 
 export const Thumbnail = () => {
   return (
-    <div className='w-full h-full  py-10 -mb-8 md:mb-0 mt-2 relative '>
+    <div className='w-full h-full  py-10 -mb-10 md:mb-0 lg:mt-4 relative '>
         <div className='max-w-[1206px] m-auto  h-full flex  flex-col  items-center justify-between' >
 
         {/* sync icon at top */}
-        <div className=' absolute -top-[24px]  w-full  flex justify-center items-center  '>
+        <div className=' absolute -top-[14px] lg:-top-[34px]  w-full  flex justify-center items-center  '>
                     <img className='sync' src={sync} sizes={60} alt="" />
                 </div>
 
-            <div className='flex flex-col md:flex-row justify-center border border-black p-4 py-8 sm:py-0 gap-4 md:gap-20 items-center'>
+            <div className='flex flex-col md:flex-row justify-center  p-4 py-0 md:py-8 sm:py-0 gap-4 md:gap-20 items-center'>
                 <div className=' md:flex  px-4 md:p-2  '>
                     <h1 className='text-black text-center'>ONE STEP, <br className='hidden md:block' /> ONE CHOICE <br className='hidden md:block' /> TO MEND THE PLANET!</h1>
                 </div>
               
               {/* Youtube-video here */}
-              <div className='sm:w-[50vw] sm:h-[22vw] w-[70vw] h-[39vw] md:w-[50vw] md:h-[22vw]  '>
+              <div className='sm:w-[50vw] mt-4 md:mt-0 shadow-md sm:h-[22vw] w-[70vw] h-[39vw] md:w-[50vw] md:h-[22vw] lg:w-[40vw] lg:h-[20vw] '>
                 <iframe
+                className='rounded-sm border border-gray-200'
                         width="100%" height="100%" src="https://www.youtube.com/embed/C80qB409rVs?si=eMKKRS5-pM_PFemG" title="YouTube video player" 
                         frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                         allowfullscreen>
