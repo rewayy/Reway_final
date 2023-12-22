a0531fe43d306cd5d58a5a78c687fb3179155a52
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-22 13:07:55 +0530

    made Cards responsive

--- src/components/Card/CardUI.jsx
+++ src/components/Card/CardUI.jsx
@@ -3,13 +3,13 @@ import './card-style.css';
 
 const Cards = (props) => {
   return (
-    <div className={`card card${props.cardId} text-center shadow`}>
-      <div className="overflow">
+    <div className={`card card${props.cardId} text-center shadow-md`}>
+      <div className="overflow ">
         <img src={props.imgsrc} alt="sgd image" className="card-img-top" />
       </div>
-      <div className="card-body text-dark">
-        <h4 className="card-title">{props.title}</h4>
-        <p className="card-text text-secondary">{props.para}</p>
+      <div className="card-body text-dark ">
+        <h4 className="card-title ">{props.title}</h4>
+        <p className="card-text text-secondary text-sm md:text-md  ">{props.para}</p>
       </div>
     </div>
   );
--- src/components/Card/FCards.jsx
+++ src/components/Card/FCards.jsx
@@ -7,33 +7,33 @@ class FCards extends Component{
     render() {
         return (
             
-            <div  className='w-full border border-black h-full bg-[#108540] bg-opacity-25 pb-14 relative  card-container'>
+            <div  className='w-full h-full bg-[#108540] bg-opacity-25 md:pb-14 relative  card-container'>
             <div className='max-w-[1206px] m-auto  h-full flex flex-col items-center justify-between'>
 
                 <div className=' absolute -top-[15px] w-full  flex justify-center items-center  '>
                     <img className='sync' src={sync} sizes={60} alt="" />
                 </div>
 
-                <div className="container-fluid flex flex-col  pt-8">
+                <div className="container-fluid flex flex-col pt-8">
                             <div className="flex justify-center items-center flex-col  ">
                                 <h1 className='text-black mb-4'> Sustainable Goals</h1>
                                 <span className=' text-black font-mont mb-8  '> Making a difference, together.</span>
                             </div>
 
-                    <div className="sdg-row p-2 mt-8">
-                        <div className="col-md-3 shadow-xl  rounded-full   ">
+                    <div className="sdg-row p-2 mt-0 md:mt-8 ">
+                        <div className="col-md-3   ">
                                 <Cards  cardId="1 " imgsrc={E_PRINT_12} title="Responsible Consumption And Production"
                                 para="Ensure sustainable consumption and production patterns." />
                         </div>
-                        <div className="col-md-3  shadow-xl  rounded-full  ">
+                        <div className="col-md-3 ">
                                 <Cards cardId="2 " imgsrc={E_WEB_13} title="Climate Action"
                                 para="Take urgent action to combat climate change and its impacts."/>
                         </div>
-                        <div className="col-md-3  shadow-xl rounded-full  ">
+                        <div className="col-md-3 ">
                                 <Cards cardId="3 " imgsrc={E_PRINT_15} title="Life on Land"
                                 para="Protect, restore and promote sustainable use of terrestial ecosystems, sustainably manage forests, combat desertification, and halt and reverse land degradation and halt biodiversity loss."/>
                         </div>
-                        <div className="col-md-3  shadow-xl  rounded-full ">
+                        <div className="col-md-3 ">
                                 <Cards cardId="4 " imgsrc={E_PRINT_17} title="Partnerships for the Goals"
                                 para="Strengthen the means of implementation and revitalize the Global Partnership for Sustainable Development. "/>
                         </div>
