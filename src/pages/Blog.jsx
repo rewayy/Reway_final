f0286548c186f478ce496885d8de5c569b7631c3
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-22 21:47:53 +0530

    modified max-min-width

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Blog = () => {
   return (
-    <div className='w-full h-screen  mt-2'>
-        <div className='max-w-[1183px] m-auto py-6 bg-gray-200  text-black  font-thin h-full flex items-center justify-center'>
+    <div className='w-full h-screen min-w-[375px]  mt-2'>
+        <div className='max-w-[1183px] lg:m-auto mx-2 py-6 md:mx-4 bg-gray-200 rounded-md text-black  font-thin h-full flex items-center justify-center'>
           Your blogs will be show here.
         </div>
     </div>
