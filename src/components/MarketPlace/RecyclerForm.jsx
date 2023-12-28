7d3f14a5c23ebc859b89e9c459b7c02ed480c984
Nikhil Yadav <facts.foundr@gmail.com>
2023-12-28 21:06:41 +0530

    updated Recycler.jsx and RecyclerForm.jsx

--- src/components/MarketPlace/RecyclerForm.jsx
+++ src/components/MarketPlace/RecyclerForm.jsx
@@ -1,17 +1,26 @@
-import React, { useState } from 'react'
-import { MdCloudUpload } from "react-icons/md";
+import React, { useState } from 'react';
+import { FaCheck } from "react-icons/fa";
+import { MdCloudUpload } from 'react-icons/md';
 import { Link, Navigate } from 'react-router-dom';
 
 const RecyclerForm = () => {
   const [quotationPage, setQuotationPage] = useState('');
+  const [quotationPdf, setQuotationPdf] = useState('');
   const [registeredNumber, setRegisteredNumber] = useState('');
   const [quotedValue, setQuotedValue] = useState('');
   const [redirect, setRedirect] = useState(false);
+  const [formError, setFormError] = useState('');
+  const [pdfUploaded, setPdfUploaded] = useState(false);
+  const [pageUploaded, setPageUploaded] = useState(false);
 
   async function handleNewRecycler(ev) {
     ev.preventDefault();
-    console.log(e.target.value)
-    
+
+    if (!registeredNumber || !quotedValue || !quotationPage || !quotationPdf) {
+      setFormError('Please fill in all the required fields.');
+      return;
+    }
+
     try {
       alert('New Recycler added successfully');
       setRedirect(true);
@@ -20,72 +29,112 @@ const RecyclerForm = () => {
     }
   }
 
+  const handlePageUpload = (ev) => {
+    setQuotationPage(ev.target.value);
+    setPageUploaded(true);
+  };
+
+  const handlePdfUpload = (ev) => {
+    setQuotationPdf(ev.target.value);
+    setPdfUploaded(true);
+  };
+
   if (redirect) {
-    return <Navigate to={'/recyclers'} />
+    return <Navigate to={'/recyclers'} />;
   }
 
-
   return (
-    <div  className='w-full h-full  min-w-[375px] md:mb-[15vh] mb-6 relative'>
-    <div className='max-w-[1183px] lg:m-auto mx-2  md:mx-4  rounded-md text-black  h-full flex  justify-center'>
-             
-            <div className='bg-green-300  bg-opacity-60 shadow-md border border-green-300 w-2/3 h-[80vh] flex  flex-col  justify-center rounded-md md:mx-20'>
-              <h1 className='text-[#008037] text-center'>Recycler form</h1>
-
-              <form action="#" onSubmit={handleNewRecycler} className='flex flex-col justify-center items-center gap-6 my-8 mx-20'>
-                <div className='flex flex-col w-full'>
-                    <label className='text-xs p-1 text-green-700'  >Quotation Page</label>
-                    <input type="text" id="quotationPage" 
-                    value={quotationPage}
-                    onChange={ev=> setQuotationPage(ev.target.value)}
-                    placeholder='Quotation page'
-                      className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4' />
-                </div>
-
-                <div className='flex flex-col w-full'>
-                    <label  className='text-xs p-1 text-green-700' >Registered Number</label>
-                    <input type="text" id="registeredNumber" 
-                    placeholder='Registered number' 
-                    value={registeredNumber}
-                    onChange={ev=> setRegisteredNumber(ev.target.value)}
-                    className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4' />
-                </div>
-
-                  <div className='w-full flex flex-col'>
-                    <label  className='text-xs p-1 text-green-700' >Total Price Quote Value (Rs.)</label>
-                    <input type="text" 
-                    id="priceQuoteValue" placeholder='Total price quote value (Rs.)' 
-                    value={quotedValue}
-                    onChange={ev=> setQuotedValue(ev.target.value)}
-                    className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4' />
-                  </div>
-
-                  <div className='w-full flex flex-col'>
-                    <label  className='text-xs p-1 text-green-700' >Total Price Quote Value (Rs.)</label>
-                    <input type="text" 
-                    id="priceQuoteValue" placeholder='Total price quote value (Rs.)' 
-                    value={quotedValue}
-                    onChange={ev=> setQuotedValue(ev.target.value)}
-                    className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4' />
-                  </div>
-
-                  {/* UPLOAD PDF */}
-                    <div className='w-full flex text-white bg-green-400 mt-4  hover:bg-green-500 py-2 rounded-md shadow-md justify-center gap-0 text-sm  flex-col items-center'>
-                      <label className='mt-1 flex  justify-center items-center gap-2 text-sm'>
-                      <input type="file" className='text-xs hidden' name="pdf" id="pdf" />
-                        <MdCloudUpload size={40} className='cursor-pointer' />
-                        Upload Quotation PDF
-                      </label>
-                    </div>
-
-                    <button className='bg-blue-500 text-white  px-8 hover:bg-blue-600 py-1 rounded-full'>Submit</button>
-              </form>
-      
+    <div className='w-full h-full min-w-[375px] md:mb-[15vh] mb-6 relative'>
+      <div className='max-w-[1183px] lg:m-auto mx-2 md:mx-4 rounded-md text-black h-full flex justify-center'>
+        <div className='bg-green-300 bg-opacity-60 shadow-md border border-green-300 w-2/3 h-full flex flex-col justify-center rounded-md md:mx-20'>
+          <h1 className='text-[#008037] text-center mt-2'>Recycler form</h1>
+          <form
+            action='#'
+            onSubmit={handleNewRecycler}
+            className='flex flex-col justify-center items-center gap-4 my-8 mx-20'
+          >
+            <div className='flex flex-col w-full'>
+              <label className='text-xs p-1 text-green-700'>Registered Number</label>
+              <input
+                type='text'
+                id='registeredNumber'
+                placeholder='Registered number'
+                value={registeredNumber}
+                onChange={(ev) => setRegisteredNumber(ev.target.value)}
+                className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4'
+              />
             </div>
-              
-    </div>
-</div>
-  )
-}
+            <div className='w-full flex flex-col'>
+              <label className='text-xs p-1 text-green-700'>Total Price Quote Value (Rs.)</label>
+              <input
+                type='text'
+                id='priceQuoteValue'
+                placeholder='Total price quote value (Rs.)'
+                value={quotedValue}
+                onChange={(ev) => setQuotedValue(ev.target.value)}
+                className='w-full shadow-md hover:shadow-lg text-start h-10  rounded-md text-xs md:text-sm px-4'
+              />
+            </div>
 
-export default RecyclerForm
+            {/* Recycler Quotation page */}
+            <div className='w-full flex text-white bg-green-500 mt-4  hover:bg-green-600 py-2 rounded-md shadow-md justify-center gap-0 text-sm  flex-col items-center'>
+              <label className='mt-1 flex  justify-center items-center gap-2 text-sm'>
+                <input
+                  type='file'
+                  id='quotationPage'
+                  value={quotationPage}
+                  onChange={handlePageUpload}
+                  className='text-xs hidden'
+                />
+                {pageUploaded ? (
+                  <>
+                    <FaCheck size={25} className='mr-2 text-white' />
+                    Quotation Page Uploaded
+                  </>
+                ) : (
+                  <>
+                    <MdCloudUpload size={40} className='cursor-pointer' />
+                    Upload Quotation Page
+                  </>
+                )}
+              </label>
+            </div>
+
+              {/* UPLOAD PDF */}
+            <div className='w-full flex text-white bg-green-500 mt-4  hover:bg-green-600 py-2 rounded-md shadow-md justify-center gap-0 text-sm  flex-col items-center'>
+              <label className='mt-1 flex  justify-center items-center gap-2 text-sm'>
+                <input
+                  type='file'
+                  id='quotationPdf'
+                  value={quotationPdf}
+                  onChange={handlePdfUpload}
+                  className='text-xs hidden'
+                />
+                {pdfUploaded ? (
+                  <>
+                    <FaCheck size={25} className=' mr-2 text-white ' />
+                    Quotation PDF Uploaded
+                  </>
+                ) : (
+                  <>
+                    <MdCloudUpload size={40} className='cursor-pointer'  />
+                    Upload Quotation PDF
+                  </>
+                )}
+              </label>
+            </div>
+          {/* to show form error if there is */}
+            {formError && <p className='text-red-500'>{formError}</p>}
+
+            {/* Submit -btn */}
+            <button className='bg-blue-600 text-white mt-4  px-8 hover:bg-blue-700 py-1 rounded-full'>
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecyclerForm;
--- src/components/MarketPlace/recyclers.jsx
+++ src/components/MarketPlace/recyclers.jsx
@@ -1,26 +1,37 @@
 import React from 'react';
 import { IoAdd } from "react-icons/io5";
 import { Link } from 'react-router-dom';
+import { MdAttachMoney } from "react-icons/md";
 
 const Recyclers = () => {
     return (
-
         <div  className='w-full h-full  min-w-[375px] md:mb-[15vh] mb-6 relative'>
             <div className='max-w-[1183px] lg:m-auto mx-2  md:mx-4 flex-col rounded-md text-black  h-screen flex   text-center'>
-
-                        <p className='text-md text-green-700 font-semibold my-2 '>Recycler page</p>
+                    <p className='text-md text-green-700 font-semibold my-2 '>Recycler page</p>
                     <div className='flex flex-col gap-4 justify-center items-center text-center m-auto w-[50vw] my-4 '>
-
                         {/* Recycler's details */}
                         <div className='flex flex-col justify-center items-center gap-4 w-full border bg-green-200 px-4 rounded-md py-6 my-2'>
-                            <p className='w-full h-8 bg-white  border rounded-md shadow-md text-sm p-1 text-slate-500'>Recycler 1 quote</p>
-                         
+                            <p className=' text-sm p-1 text-slate-600'>Recycler's quotations are shown here..</p>
+                            {/* table heading */}
+                                <div className='w-full flex flex-col gap-1'>
+                                    <div className='w-full grid grid-cols-4 items-center relative h-10  bg-green-600 bg-opacity-80 text-white rounded-md shadow-md'>
+                                        <div className=' text-md p-1 border-r-2 '>S.No.</div>
+                                        <div className=' text-md p-1  border-r-2 '>Price PDF</div>
+                                        <div className=' text-sm p-1 border-r-2  '>Price (Rs.)</div>
+                                        <div className=' text-sm p-1 '>Button</div>
+                                    </div>
+                                    {/* Fetched data div */}
+                                    <div className='bg-white grid grid-cols-4 rounded-md w-full '>
+                                        <div className='text-sm border-slate-300 text-slate-500 p-2 border-r-2'>1.</div>
+                                        <div className='text-sm border-slate-300 text-slate-500 p-2 border-r-2'> pdf</div>
+                                        <div className='text-sm border-slate-300 text-slate-500  p-2 border-r-2'>Rs. 50 k</div>
+                                        <div className='text-sm flex  justify-center items-center  p-21 border-r-2 text-slate-500'>
+                                            <Link to={'/recycler-form'} className='bg-blue-600 hover:bg-blue-700  text-center text-white rounded-md py-1 w-full mx-2'>Confirm</Link>
+                                        </div>
+                                    </div>
+                                </div>
                         </div>
-
-                        <Link to={'/recycler-form'} className='bg-green-600 flex  justify-center items-center gap-2 font-mont hover:bg-green-700  px-4 py-2 rounded-full text-white text-sm'>  <IoAdd size={25} /> Add Recycler</Link>
-                        
                     </div>
-
             </div>
         </div>
 
@@ -29,4 +40,14 @@ const Recyclers = () => {
 
 export default Recyclers ;
 
+// Bid btn
+{/* <button className='bg-blue-600  border-4 flex absolute right-0 top-0justify-center items-center hover:bg-blue-700 px-2 text-sm py-2  text-white rounded-md'>
+    Bid
+<MdAttachMoney size={24}/>
+</button> */}
 
+// Add btn
+{/* <Link to={'/recycler-form'} className='bg-green-600 flex  justify-center items-center gap-2 font-mont hover:bg-green-700  px-4 py-2 rounded-full text-white text-sm'>  
+<IoAdd size={25} /> 
+Add Recycler
+</Link> */}
